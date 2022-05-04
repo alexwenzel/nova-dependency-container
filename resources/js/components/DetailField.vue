57ede14649e3fc72c8c5badfa58759ea35a83f5e
Alexander Wenzel <alexander.wenzel.berlin@gmail.com>
2022-05-04 19:57:39 +0200

    prototyped plugin, migrated to vue3 and nova 4

--- resources/js/components/DetailField.vue
+++ resources/js/components/DetailField.vue
@@ -1,9 +1,44 @@
 <template>
-    <PanelItem :index="index" :field="field"/>
+    <div v-if="dependenciesSatisfied">
+        <div v-for="childField in field.fields">
+            <component
+                :is="'detail-' + childField.component"
+                :resource-id="resourceId"
+                :resource-name="resourceName"
+                :field="childField"
+                :ref="'field-' + childField.attribute"
+            />
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-    props: ['index', 'resource', 'resourceName', 'resourceId', 'field'],
+    props: ['resource', 'resourceName', 'resourceId', 'field'],
+
+    created() {
+        this.updateDependencyStatus()
+    },
+
+    data() {
+        return {
+            dependenciesSatisfied: false,
+        }
+    },
+
+    methods: {
+
+        updateDependencyStatus() {
+            for (let dependency of this.field.dependencies) {
+                if (dependency.satisfied) {
+                    this.dependenciesSatisfied = true;
+                    return;
+                }
+            }
+
+            this.dependenciesSatisfied = false;
+        },
+
+    }
 }
 </script>
